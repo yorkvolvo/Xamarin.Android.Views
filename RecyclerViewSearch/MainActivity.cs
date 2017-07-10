840b701e0851b6fb6403f0457dd92a60344ac03b
York Shen <v-shenya@microsoft.com>
2017-07-10 17:53:03 +0800

    Add project files.

---
+++ RecyclerViewSearch/MainActivity.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Runtime;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+
+namespace RecyclerViewSearch
+{
+    [Activity(Label = "RecyclerViewSearch", MainLauncher = true, Icon = "@drawable/icon",Theme = "@style/Theme.AppCompat.Light")]
+    public class MainActivity : ActionBarActivity
+    {
+
+        private Android.Support.V7.Widget.SearchView _searchView;
+        private RecyclerView _recyclerView;
+        RecyclerView.LayoutManager mLayoutManager;
+        private RecyclerViewAdapter _adapter;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            SupportActionBar.SetDisplayShowHomeEnabled(true);
+
+            var chemicals = new List<Chemical>
+            {
+                new Chemical {Name = "Niacin", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Biotin", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Chromichlorid", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Natriumselenit", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Manganosulfate", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Natriummolybdate", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Ergocalciferol", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Cyanocobalamin", DrawableId = Resource.Drawable.Icon},
+            };
+
+            _recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            _adapter = new RecyclerViewAdapter(this,chemicals);
+            mLayoutManager = new LinearLayoutManager(this);
+
+            _recyclerView.SetLayoutManager(mLayoutManager);
+            _recyclerView.SetAdapter(_adapter);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.main, menu);
+
+            var item = menu.FindItem(Resource.Id.action_search);
+
+            var searchView = MenuItemCompat.GetActionView(item);
+            _searchView = searchView.JavaCast<Android.Support.V7.Widget.SearchView>();
+
+            _searchView.QueryTextChange += (s, e) => _adapter.Filter.InvokeFilter(e.NewText);
+
+            _searchView.QueryTextSubmit += (s, e) =>
+            {
+                // Handle enter/search button on keyboard here
+                Toast.MakeText(this, "Searched for: " + e.Query, ToastLength.Short).Show();
+                e.Handled = true;
+            };
+
+            MenuItemCompat.SetOnActionExpandListener(item, new SearchViewExpandListener(_adapter));
+
+            return true;
+        }
+
+        private class SearchViewExpandListener : Java.Lang.Object, MenuItemCompat.IOnActionExpandListener
+        {
+            private readonly IFilterable _adapter;
+
+            public SearchViewExpandListener(IFilterable adapter)
+            {
+                _adapter = adapter;
+            }
+
+            public bool OnMenuItemActionCollapse(IMenuItem item)
+            {
+                _adapter.Filter.InvokeFilter("");
+                return true;
+            }
+
+            public bool OnMenuItemActionExpand(IMenuItem item)
+            {
+                return true;
+            }
+        }
+    }
+}
+
---
+++ RecyclerViewSearch/RecyclerViewAdapter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using Java.Lang;
+
+namespace RecyclerViewSearch
+{
+    public class RecyclerViewAdapter : RecyclerView.Adapter, IFilterable
+    {
+        private List<Chemical> _originalData;
+        private List<Chemical> _items;
+        private readonly Activity _context;
+
+        public Filter Filter { get; private set; }
+
+        public RecyclerViewAdapter(Activity activity, IEnumerable<Chemical> chemicals)
+        {
+            _items = chemicals.OrderBy(s => s.Name).ToList();
+            _context = activity;
+
+            Filter = new ChemicalFilter(this);
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.Chemical, parent, false);
+            ChemicalHolder vh = new ChemicalHolder(itemView);
+            return vh;
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            ChemicalHolder vh = holder as ChemicalHolder;
+
+            var chemical = _items[position];
+
+            vh.Image.SetImageResource(chemical.DrawableId);
+            vh.Caption.Text = chemical.Name;
+        }
+
+        public override int ItemCount
+        {
+            get { return _items.Count; }
+        }
+
+        public class ChemicalHolder : RecyclerView.ViewHolder
+        {
+            public ImageView Image { get; private set; }
+            public TextView Caption { get; private set; }
+
+            public ChemicalHolder(View itemView) : base(itemView)
+            {
+                Image = itemView.FindViewById<ImageView>(Resource.Id.chemImage);
+                Caption = itemView.FindViewById<TextView>(Resource.Id.chemName);
+            }
+        }
+
+        private class ChemicalFilter : Filter
+        {
+            private readonly RecyclerViewAdapter _adapter;
+            public ChemicalFilter(RecyclerViewAdapter adapter)
+            {
+                _adapter = adapter;
+            }
+
+            protected override FilterResults PerformFiltering(ICharSequence constraint)
+            {
+                var returnObj = new FilterResults();
+                var results = new List<Chemical>();
+                if (_adapter._originalData == null)
+                    _adapter._originalData = _adapter._items;
+                
+                if (constraint == null) return returnObj;
+
+                if (_adapter._originalData != null && _adapter._originalData.Any())
+                {
+                    // Compare constraint to all names lowercased. 
+                    // It they are contained they are added to results.
+                    results.AddRange(
+                        _adapter._originalData.Where(
+                            chemical => chemical.Name.ToLower().Contains(constraint.ToString())));
+                }
+                
+                // Nasty piece of .NET to Java wrapping, be careful with this!
+                returnObj.Values = FromArray(results.Select(r => r.ToJavaObject()).ToArray());
+                returnObj.Count = results.Count;
+
+                constraint.Dispose();
+
+                return returnObj;
+            }
+
+            protected override void PublishResults(ICharSequence constraint, FilterResults results)
+            {
+                using (var values = results.Values)
+                    _adapter._items = values.ToArray<Java.Lang.Object>()
+                        .Select(r => r.ToNetObject<Chemical>()).ToList();
+                    
+                _adapter.NotifyDataSetChanged();
+
+                // Don't do this and see GREF counts rising
+                constraint.Dispose();
+                results.Dispose();
+            }
+        }
+    }
+
+}
